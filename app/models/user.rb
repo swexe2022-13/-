16741683d9a10a847bc18f7bf7f9b578b9e98956
2011140040 <ubuntu@ip-172-31-42-71.ec2.internal>
2022-11-30 09:07:41 +0000

    top_controllerのloginアクションにpass: params[:pass]追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
+    attr_accessor :password, :password_confirmation
     
+    def password=(val)
+        if val.present?
+            pass=BCrypt::Password.create(val)
+        end
+        @password=val
+    end
 end
