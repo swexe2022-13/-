b76a4014843c9fd85797536c2e7f7be78c5ed52f
Ubuntu <ubuntu@ip-172-31-42-71.ec2.internal>
2022-11-30 06:56:49 +0000

    共有

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   get 'users/new'
   get 'users/create'
   get 'users/destroy'
+  
   get 'top/main'
+  post 'top/login'
   get 'top/login'
   get 'top/logout'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
